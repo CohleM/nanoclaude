7f834f57c25f72575f4f3a1ad33672761c828594
CohleM <manish981194@gmail.com>
2026-05-10 14:27:18 +0545

    step3.1

--- nanoclaude.py
+++ nanoclaude.py
@@ -35,6 +35,52 @@ get_weather_tool = {
     }
 }
 
+_DETAILED_BASH_DESCRIPTION = """Execute a bash command in the terminal within a persistent shell session.
+
+### Command Execution
+* One command at a time: You can only execute one bash command at a time. If you need to run multiple commands sequentially, use `&&` or `;` to chain them together.
+* Persistent session: Commands execute in a persistent shell session where environment variables, virtual environments, and working directory persist between commands.
+* Timeout: Commands have a soft timeout of 10 seconds, once that's reached, you have the option to continue or interrupt the command (see section below for details)
+
+### Running and Interacting with Processes
+* Long running commands: For commands that may run indefinitely, run them in the background and redirect output to a file, e.g. `python3 app.py > server.log 2>&1 &`. For commands that need to run for a specific duration, like "sleep", you can set the "timeout" argument to specify a hard timeout in seconds.
+* Interact with running process: If a bash command returns exit code `-1`, this means the process is not yet finished. By setting `is_input` to `true`, you can:
+  - Send empty `command` to retrieve additional logs
+  - Send text (set `command` to the text) to STDIN of the running process
+  - Send control commands like `C-c` (Ctrl+C), `C-d` (Ctrl+D), or `C-z` (Ctrl+Z) to interrupt the process
+
+### Best Practices
+* Directory verification: Before creating new directories or files, first verify the parent directory exists and is the correct location.
+* Directory management: Try to maintain working directory by using absolute paths and avoiding excessive use of `cd`.
+
+### Output Handling
+* Output truncation: If the output exceeds a maximum length, it will be truncated before being returned.
+"""
+
+
+def bash_tool() -> dict:
+    
+    return {
+        'type': 'function',
+        'function': {
+            'name': 'execute_bash',
+            'description': _DETAILED_BASH_DESCRIPTION,
+            'parameters': {
+                'type': 'object',
+                'properties': {
+                    'command': {
+                        'type': 'string',
+                        'description': 
+                            'The bash command to execute. Can be empty string to view additional logs when previous exit code is `-1`. Can be `C-c` (Ctrl+C) to interrupt the currently running process. Note: You can only execute one bash command at a time. If you need to run multiple commands sequentially, you can use `&&` or `;` to chain them together.'
+                        ,
+                    },
+                    
+                },
+                'required': ['command'],
+            },
+        },
+    }
+
 
 def get_weather_fn(location):
     return str({"weather": "Sunny", "temp": "28C", "location": location})
@@ -46,7 +92,7 @@ class NanoClaude:
         self.history = [{"role": "system", "content": "You're NanoClaude Code, a god tier coding agent"}]
         
         # ADD TOOLs
-        self.tools = [get_weather_tool]
+        self.tools = [bash_tool()]
     def step(self):
         params = {
             **Config().model_dump(),
@@ -70,22 +116,29 @@ class NanoClaude:
 
             if assistant_msg.content:
                 print('='*50)
-                print('ASSISTANT\n', assistant_msg.content, )
+                print('ASSISTANT\n', assistant_msg.content )
                 print('='*50)
 
             # ADD TOOLS
             if hasattr(assistant_msg, "tool_calls") and assistant_msg.tool_calls:
                 self.history.append(assistant_msg.model_dump())
                 for tool in assistant_msg.tool_calls:
-                    if tool.function.name == 'get_weather':
-                        location = json.loads(tool.function.arguments)['location']
-                        output = get_weather_fn(location)
+
+
+                    # if tool.function.name == 'get_weather':
+                    #     location = json.loads(tool.function.arguments)['location']
+                    #     output = get_weather_fn(location)
                         
-                        self.history.append({
-                        "role": "tool",
-                        "content": output,
-                        "tool_call_id": tool.id
-                        })
+                    #     self.history.append({
+                    #     "role": "tool",
+                    #     "content": output,
+                    #     "tool_call_id": tool.id
+                    #     })
+
+                    observation = self.perform_action(tool)
+                    self.history.append(observation)
+
+
                         
 
             # natural stop
@@ -94,6 +147,19 @@ class NanoClaude:
                 print("Execution Finished")
                 print('='*50)
                 break
+    
+    def perform_action(self, tool):
+        tool_name = tool.function.name
+
+        if tool_name == "execute_bash":
+            arguments = json.loads(tool.function.arguments)
+            command = arguments.get("command", "")
+
+            # now we need a bash session to execute this command
+            # and return that command's output back to our LLM
+            pass
+            
+
 
     # ── interactive repl ────────────────────────────────────────────────
     def repl(self):
